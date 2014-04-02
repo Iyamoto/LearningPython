b74273b9a7f3e48cfbc9d2f849be48dcc16d4e0d
Iyamoto <Iyamoto@users.noreply.github.com>
2014-04-02 11:20:06 +0400

    more and more

--- anomaly0/gyp.py
+++ anomaly0/gyp.py
@@ -3,6 +3,8 @@ import os
 import urllib.request
 from urllib.parse import urlparse
 import hashlib
+import subprocess
+import socket
 
 def file2text(path):
     """Reads utf-8 file from path"""    
@@ -51,20 +53,109 @@ def getID(s):
     id = hashlib.md5(s.encode('utf-8')).hexdigest()
     return id
 
-def getSize(items):
+def getIndex1(items):
     return items[1]
+def getIndex2(items):
+    return items[2]
 
 def showData(data):
     for item in data:
-        print(item[0],item[1])
-    return None    
+        print(item)
+    return None
+
+def getUniq(filepath):
+    cmd = 'wc '+filepath
+    output=subprocess.check_output(cmd, shell=True)
+    uniq = int(output.split()[0])
+    #print(uniq)
+    return uniq
+
+def getWhois(domain, cachedir):
+    cachefile = os.path.join(cachedir, domain)
+    if os.path.isfile(cachefile) == True:
+        lines = file2list(cachefile)
+    else:
+        cmd = 'whois '+domain
+        output=subprocess.check_output(cmd, shell=True)
+        s = output.decode()
+        lines = s.splitlines()
+        f = open(cachefile,'w')
+        f.write(s)
+        f.close()
+    return lines
+
+def getCreationDate(domain, cachedir):
+    lines = getWhois(domain, cachedir)
+    date = ''
+    for line in lines:
+        if line.find('Creation Date')>=0:
+            date = line.split(':')[1].strip()
+    return date
+
+def getIP(host):
+    try:
+        ips = socket.gethostbyname_ex(host)
+    except socket.gaierror:
+        ips=[]
+    return ips[2][0]
+
+def getIndex(url, indexfile):
+    url = url.strip()
+    parts = url.split('/')
+    url = ''
+    for part in parts[:-1]:
+        url+=part+'/'
+    if os.path.isfile(indexfile) == False:
+        try:
+            urllib.request.urlretrieve(url, indexfile)          
+        except urllib.error.HTTPError:
+            print('error')
+    print(url)
+    return None
+
+def isDate(part):
+    if part.count('-')==2:
+        return True
+    else:
+        return False
+
+def getFileDate(indexfile):
+    lines = file2list(indexfile)
+    for line in lines:
+        if line.find('data.txt')>=0:
+            parts = line.strip().split()
+            for part in parts:
+                if isDate(part):
+                    return part
+    return None
+
+def save2csv(data, filepath, sep=';'):
+    f = open(filepath,'w')
+    top = 'id;Domain;size;uniqs;domain creation_date;ip;file_date;\n'
+    f.write(top)
+    for items in data:
+        s = ''
+        for item in items:
+            s=s+str(item)+sep
+        f.write(s+'\n')
+    f.close()
+    return    
 
 #saveUrlsfromSerp('urls.txt')
 
 urls = file2list('urls.txt')
 urls.sort()
 print(len(urls))
-datadir = 'R:\\lucid\\anomaly\\0\\data\\'
+
+basedir = 'R:\\lucid\\anomaly\\0\\'
+datadir = os.path.join(basedir, 'data')
+sorteddir = os.path.join(basedir, 'sorted')
+uniqdir = os.path.join(basedir, 'uniq')
+cachedir = os.path.join(basedir, 'cache')
+indexdir = os.path.join(basedir, 'indexes')
+
+csvfile = 'save.csv'
+
 data = []
 
 for url in urls:
@@ -74,6 +165,7 @@ for url in urls:
     # TODO get domain whois
     # TODO get IP
     datafile = os.path.join(datadir, id)
+    # Retrieving
 ##    if os.path.isfile(datafile) == False:
 ##        try:
 ##            urllib.request.urlretrieve(url, datafile)          
@@ -82,10 +174,31 @@ for url in urls:
 ##            continue
     if os.path.isfile(datafile) == True:
         size = os.path.getsize(datafile)
-        #print(domain, size)
-        data.append((domain, size))
-                
-showData(sorted(data,key=getSize, reverse=True))
+        sortedfile = os.path.join(sorteddir, id)
+        uniqfile = os.path.join(uniqdir, id)
+        indexfile = os.path.join(indexdir, id)
+        # Sorting
+##        cmd = 'sort '+datafile+' > '+sortedfile
+##        subprocess.call(cmd, shell=True)
+        # Uniq
+##        cmd = 'uniq '+sortedfile+' > '+uniqfile
+##        subprocess.call(cmd, shell=True)
+        uniq = getUniq(uniqfile)
+        creation_date = getCreationDate(domain, cachedir)
+        ip = getIP(domain)
+        # Get indexes
+        #getIndex(url, indexfile)
+        if os.path.isfile(indexfile) == True:
+            file_date = getFileDate(indexfile)
+        else:
+            file_date = None
+        #break
+        data.append((id, domain, size, uniq, creation_date, ip, file_date))
+
+#print(('id','Domain','size','uniqs', 'domain creation_date', 'ip', 'file_date'))                
+#showData(sorted(data,key=getIndex1, reverse=True))
+# TODO save to csv
+save2csv(sorted(data,key=getIndex2, reverse=True),csvfile)
 print('done')            
     
         
