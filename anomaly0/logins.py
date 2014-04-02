f0391dedc305b2922a4c8d27ec6003082e1d3d0a
Iyamoto <Iyamoto@users.noreply.github.com>
2014-04-02 13:16:04 +0400

    logins

---
+++ anomaly0/logins.py
@@ -0,0 +1,44 @@
+import os
+id = 'cf15cb26bdb1a9eb0fdaa747b0105a19'
+basedir = 'R:\\lucid\\anomaly\\0\\'
+uniqdir = os.path.join(basedir, 'uniq')
+loginsdir = os.path.join(basedir, 'logins')
+grep = os.path.join(uniqdir, id)
+out = os.path.join(loginsdir, id)
+
+f = open(grep,'rU')
+count = {}
+good = {}
+last = ''
+
+for line in f:
+    pair = line.strip().split(':')
+    if len(pair)==2:
+        login = pair[0].lower()
+        if len(login)==0:
+            continue
+        if login!=last:            
+            pas = pair[1]
+            if len(pas)<10:
+                continue
+            if login==pas:
+                continue
+            count[login] = count.get(login, 0) + 1
+            #print(login, last,count[login])
+            if count[login]==1:
+                good[pair[0]] = pas
+                last = login
+            elif count[login]>1:
+                last = login
+                del(good[last])
+
+f.close()    
+
+#print(good)
+
+f = open(out,'w')
+for k,v in good.items():
+    f.write(k+':'+v+'\n')
+
+f.close()    
+    
